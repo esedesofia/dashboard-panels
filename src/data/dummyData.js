bd0bbb3e5cbac4f3c38f792f24bb74e5153fcb84
esedesofia <sperezhidd@gmail.com>
2025-01-04 18:15:41 -0300

    feat: add dummy data

---
+++ src/data/dummyData.js
@@ -0,0 +1,24 @@
+const solarData = {
+  energyGenerated: [
+    { date: "2024-01-01", watts: 4500 },
+    { date: "2024-01-02", watts: 5000 },
+    { date: "2024-01-03", watts: 4700 },
+    { date: "2024-01-04", watts: 4800 },
+    { date: "2024-01-05", watts: 4900 },
+    { date: "2024-01-06", watts: 4600 },
+    { date: "2024-01-07", watts: 4750 },
+    { date: "2024-01-08", watts: 5000 },
+    { date: "2024-01-09", watts: 5100 },
+    { date: "2024-01-10", watts: 4950 },
+    { date: "2024-01-11", watts: 5200 },
+    { date: "2024-01-12", watts: 5300 },
+    { date: "2024-01-13", watts: 5100 },
+    { date: "2024-01-14", watts: 4800 },
+    { date: "2024-01-15", watts: 4700 },
+  ],
+  panelAngle: 35,
+  efficiency: 95,
+  panelPower: 4000,
+};
+
+export default solarData;
